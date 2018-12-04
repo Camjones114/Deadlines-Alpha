ab3c52e8a16c026a86ed72c1f67d1f8255ad690f
Nicholas Harding <nharding@csumb.edu>
2018-12-04 12:39:52 -0800

    Adding my stuff and Cameron's stuff.

---
+++ Deadlines Alpha 2.1/Assets/Deadlines Assets/Scripts/AI Scripts/EnemyMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public Transform playerPos;
+
+	private Rigidbody2D myRigidBody;
+	private float movementspeed;
+	private bool facing = true;
+	private float horizontal;
+
+	private float distFromPlayer;
+
+
+	public void calcRecDist(string playerType){
+		/* Gets the type of player from somewhere. The name of the object seems to make the most sense, as tag is already being
+		 * used to identify what the player is in the basicEnemy script. These are tentative, to be finalized when the name
+		 * of the player objects is finalized.
+		 */
+
+		if (playerType == "Nerd") {
+			//Recommended distance is lower, because the Nerd has ranged attacks.
+			distFromPlayer = 1f;
+		}
+
+		if (playerType == "Athlete") {
+			//Recommended distance is higher, because the Athlete has mostly meelee attacks.
+			distFromPlayer = 4f;
+		}
+
+		if (playerType == "Slacker") {
+			//Not sure what the recommended distance for the slacker should be.
+			distFromPlayer = 2f;
+		}
+	}
+
+
+	public float getMovementspeed(){
+		return movementspeed;
+	}
+
+	public void setMovementspeed(float ms){
+		movementspeed = ms;
+	}
+
+	public void setPlayerLoc(Transform t){
+		//How we figure out where the player is so we can go there.
+		playerPos = t;
+	}
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (playerPos.position.x >= gameObject.transform.position.x) {
+			horizontal = 1;
+		} else {
+			horizontal = -1;
+		}
+
+		myRigidBody.velocity = new Vector2(horizontal * movementspeed, myRigidBody.velocity.y);
+
+		if (horizontal > 0){
+			if (!facing){
+				Flip();
+			}
+		}
+		else if (horizontal < 0 ){
+			if (facing){
+				Flip();
+			}
+		}
+	}
+
+	private void Flip() {
+		facing = !facing;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
+
--- Deadlines Alpha 2.1/Assets/Deadlines Assets/Scripts/AI Scripts/basicEnemy.cs
+++ Deadlines Alpha 2.1/Assets/Deadlines Assets/Scripts/AI Scripts/basicEnemy.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class basicEnemy : MonoBehaviour {
 
+	public int damage;
+	public int health;
+	public GameObject player;
+
+	public EnemyMovement movement;
+
+
+
 	// Use this for initialization
 	void Start () {
-		
+		movement = gameObject.GetComponent<EnemyMovement> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+		//These would go in a specific enemy class, but these are for testing purposes.
+		movement.setMovementspeed (4f);
+
+		//These are the same for every enemy type, so they'll stay here.
+		movement.calcRecDist(player.name);
+		movement.setPlayerLoc (player.transform);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 }
