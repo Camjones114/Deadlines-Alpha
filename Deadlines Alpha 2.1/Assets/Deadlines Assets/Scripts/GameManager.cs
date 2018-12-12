016649eae3ae82ebc10c13da3d76ab36149436a0
Cody Regalado <hakarushokku@gmail.com>
2018-12-12 01:42:33 -0800

    updating the UI

--- Deadlines Alpha 2.1/Assets/Deadlines Assets/Scripts/GameManager.cs
+++ Deadlines Alpha 2.1/Assets/Deadlines Assets/Scripts/GameManager.cs
@@ -3,82 +3,119 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	[SerializeField]
-	private player character;
-	[SerializeField]
-	private int numLives;
-	private int score;
-	private int coinCount;
-	private int healthAmt;
+    [SerializeField]
+    private player character;
+    [SerializeField]
+    private int numLives;
+    [SerializeField]
+    private int coinCount;
+    [SerializeField]
+    private int healthAmt;
+    private HealthDisplay healthHolder;
 
-	[SerializeField]
-	private float timeForTimer;
-	private Scene level;
+    private Scene level;
 
-	// Use this for initialization
-	void Start () {
-		character = FindObjectOfType<player> ();
-		if (character == null) {
-			Debug.LogWarning ("Character not found.");
-			character = FindObjectOfType<player> ();
-		}
-		healthAmt = character.getHealth ();
-		//Todo: Find out how to save the scene of the level that was created procedurally,
-		// so that it may be loaded upon death.
+    // Use this for initialization
+    void Start()
+    {
+        healthHolder = FindObjectOfType<HealthDisplay>();
+        character = FindObjectOfType<player>();
+        if (character == null)
+        {
+            Debug.LogWarning("Character not found.");
+            character = FindObjectOfType<player>();
+        }
+        //Todo: Find out how to save the scene of the level that was created procedurally,
+        // so that it may be loaded upon death.
+        numLives = character.getLives();
 
-		FindObjectOfType<TimerDisplay>().setTimer(getTimeForLevel ());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		if (healthAmt < 0) {
-			decrementLives ();
-		}
+        FindObjectOfType<TimerDisplay>().setTimer(getTimeForLevel());
+    }
 
-		if (coinCount > 9) {
-			incrementLives ();
-			coinCount -= 10;
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        healthAmt = healthHolder.getHealth();
 
-		if (numLives < 0) {
-			SceneManager.LoadScene ("GameOver");
-		}
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            decrementLives();
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+            incrementLives();
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            addCoins(10);
+        }
 
-	}
+        if (healthAmt <= 0)
+        {
+            decrementLives();
+            healthHolder.recoverHealth(character.getHealth());
+        }
 
-	float getTimeForLevel(){
-		string playerType = FindObjectOfType<player> ().GetType ().ToString ();
-		switch (playerType) {
-		case "athlete":
-			return 270.0f;
-			break;
-		case "nerd":
-			return 330.0f;
-			break;
-		case "procrastinator":
-			return 300.0f;
-			break;
-		default:
-			Debug.LogError ("Player type not found, setting default time.");
-			return 1000.0f;
-			break;
-		}
-	}
+        if (coinCount > 24)
+        {
+            incrementLives();
+            coinCount -= 25;
+        }
+        if(coinCount  < 0)
+        {
+            coinCount = 0;
+        }
 
-	void decrementLives(){
-		numLives--;
-		FindObjectOfType<LivesDisplay> ().updateLives ();
-	}
+        if (numLives < 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
 
-	void incrementLives(){
-		numLives++;
-		FindObjectOfType<LivesDisplay> ().updateLives ();
-	}
+    }
 
-	public int getNumLives(){
-		return numLives;
-	}
+    float getTimeForLevel()
+    {
+        string playerType = FindObjectOfType<player>().GetType().ToString();
+        switch (playerType)
+        {
+            case "athlete":
+                return 270.0f;
+                break;
+            case "nerd":
+                return 330.0f;
+                break;
+            case "procrastinator":
+                return 300.0f;
+                break;
+            default:
+                Debug.LogError("Player type not found, setting default time.");
+                return 1000.0f;
+                break;
+        }
+    }
+
+    public void addCoins(int amt)
+    {
+        coinCount += amt;
+    }
+
+    public void decrementLives()
+    {
+        numLives--;
+        FindObjectOfType<LivesDisplay>().updateLives();
+    }
+
+    public void incrementLives()
+    {
+        numLives++;
+        FindObjectOfType<LivesDisplay>().updateLives();
+    }
+
+    public int getNumLives()
+    {
+        return numLives;
+    }
 }
--- Deadlines Alpha 2.1/Assets/Deadlines Assets/Scripts/UI Scripts/HealthDisplay.cs
+++ Deadlines Alpha 2.1/Assets/Deadlines Assets/Scripts/UI Scripts/HealthDisplay.cs
@@ -2,34 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth;
+    private int health;
+    public Text healthText;
+    private bool updateHealth;
+    private GameObject character;
+    [SerializeField]
+    private RectTransform healthBar;
+    void Start()
+    {
+        character = GameObject.FindGameObjectWithTag("Player");
+        maxHealth = character.GetComponent<player>().getHealth();
+        health = maxHealth;
+        healthText = GetComponent<Text>();
+        healthText.text = "Health : " + health;
+        updateHealth = false;
+        if (healthBar == null)
+        {
+            Debug.Log("Status indicator: No health bar referenced.");
+        }
+    }
+    //update the health based on an increase or decrease 
+    public void recoverHealth(int amount)
+    {
+        health += amount;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        updateHealth = true;
+    }
+    public void takeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            health = 0;
+        }
+        updateHealth = true;
+    }
 
-public class HealthDisplay : MonoBehaviour {
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            takeDamage(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
+            recoverHealth(1);
+        }
+        if (updateHealth)
+        {
+            healthText.text = "Health : " + health;
+            float value = (float)health / (float)maxHealth;
+            healthBar.localScale = (new Vector3(value, 1f, 1f));
+        }
+    }
 
-	[SerializeField]
-	private int health;
-	public Text healthText;
-	private bool updateHealth;
-	private GameObject character;
-
-	void Start(){
-		character = GameObject.FindGameObjectWithTag ("Player");
-		health = character.GetComponent<player>().getHealth();
-		healthText = GetComponent<Text> ();
-		healthText.text = "Health : " + health;
-		updateHealth = false;
-	}
-
-	//update the health based on an increase or decrease 
-	void changeHealth(int amount){
-		health += amount;
-		updateHealth = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (updateHealth) {
-			healthText.text = "Health : " + health;
-		}
-
-	}
+    public int getHealth()
+    {
+        return health;
+    }
 }
